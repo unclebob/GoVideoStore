cf385943108fe3488f74ef0a879dd2b7bf916f6a
Robert C. Martin <unclebob@cleancoder.com>
2024-08-16 07:49:23 -0500

    Move determine functions to Rental

--- Customer.go
+++ Customer.go
@@ -42,36 +42,10 @@ func (customer *Customer) makeDetails() string {
 }
 
 func (customer *Customer) makeDetail(rental *Rental) string {
-	amount := customer.determineAmount(rental)
-	customer.frequentRenterPoints += customer.determinePoints(rental)
+	amount := rental.determineAmount()
+	customer.frequentRenterPoints += rental.determinePoints()
 	customer.totalAmount += amount
 	return customer.formatDetail(rental, amount)
-}
-
-func (customer *Customer) determineAmount(rental *Rental) float64 {
-	amount := 0.0
-	switch rental.movie.movieType {
-	case NewRelease:
-		amount += float64(rental.daysRented * 3)
-	case Regular:
-		amount += 2
-		if rental.daysRented > 2 {
-			amount += float64(rental.daysRented-2) * 1.5
-		}
-	case Childrens:
-		amount += 1.5
-		if rental.daysRented > 3 {
-			amount += float64(rental.daysRented-3) * 1.5
-		}
-	}
-	return amount
-}
-
-func (customer *Customer) determinePoints(rental *Rental) int {
-	if rental.movie.movieType == NewRelease && rental.daysRented > 1 {
-		return 2
-	}
-	return 1
 }
 
 func (customer *Customer) formatDetail(rental *Rental, amount float64) string {
--- Rental.go
+++ Rental.go
@@ -9,6 +9,32 @@ func (rental Rental) getTitle() string {
 	return rental.movie.title
 }
 
+func (rental Rental) determineAmount() float64 {
+	amount := 0.0
+	switch rental.movie.movieType {
+	case NewRelease:
+		amount += float64(rental.daysRented * 3)
+	case Regular:
+		amount += 2
+		if rental.daysRented > 2 {
+			amount += float64(rental.daysRented-2) * 1.5
+		}
+	case Childrens:
+		amount += 1.5
+		if rental.daysRented > 3 {
+			amount += float64(rental.daysRented-3) * 1.5
+		}
+	}
+	return amount
+}
+
+func (rental Rental) determinePoints() int {
+	if rental.movie.movieType == NewRelease && rental.daysRented > 1 {
+		return 2
+	}
+	return 1
+}
+
 func NewRental(movie *Movie, daysRented int) *Rental {
 	return &Rental{movie: movie, daysRented: daysRented}
 }
